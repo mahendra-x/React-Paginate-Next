8e10b1860d01ce36ddeebf46e4aae1813df6fef3
mahendra-x <ymahendra1995s@gmail.com>
2025-02-18 18:15:47 +0530

    added autocomplete search

---
+++ src/app/autoSearchBar/page.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { useEffect, useState } from "react";
+import "./styles.css";
+import { redirect } from "next/navigation";
+
+export default function AutoSearchBar() {
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState([]);
+  const [showresults, setShowResults] = useState(false);
+  const [cache, setCache] = useState({});
+
+  const fetchData = async () => {
+    if (cache[input]) {
+      console.log("CACHE RETURED VALUE", input);
+      setResults(cache[input]);
+      return;
+    }
+    // console.log("API CALL", input);
+    const data = await fetch("https://dummyjson.com/recipes/search?q=" + input);
+    const results = await data.json();
+    // console.log("first", results);
+    setResults(results.recipes);
+    setCache((prev) => ({
+      ...prev,
+      [input]: results.recipes,
+    }));
+  };
+
+  useEffect(() => {
+    if (input) {
+      console.log("input", input);
+    }
+    const timer = setTimeout(fetchData, 500);
+    // fetchData();
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
+
+  const backToHomeHandler = () => {
+    redirect("/");
+  };
+
+  //   console.log("cache", cache);
+  return (
+    <>
+      <button onClick={backToHomeHandler} className="text-2xl p-5 text-left">
+        ⬅️ back to home page
+      </button>
+      <div className="App flex justify-center align-middle">
+        <h1 className="font-bold text-2xl">AutoComplete Search Bar</h1>
+        <div>
+          <input
+            type={"text"}
+            className="search-input border border-cyan-500"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onFocus={() => setShowResults(true)}
+            onBlur={() => setShowResults(false)}
+            placeholder="Click here for Search"
+          />
+
+          {showresults && (
+            <div className="result-container">
+              {results.map((item, index) => (
+                <span key={item.id} className="result">
+                  {item.name}
+                </span>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,18 @@ export default function Home() {
     // alert("hi");
     redirect(`/tabForms`); // Navigate to the new post page
   };
+  const handleSearchBar = () => {
+    // alert("hi");
+    redirect(`/autoSearchBar`); // Navigate to the new post page
+  };
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h3>Home Page</h3>
         <button onClick={nextBtnHandler} className="border-2 p-2 bg-slate-500 text-white border-cyan-400">Please Click for Pagination View</button>
         <button onClick={handleTabForms} className="border-2 p-2 bg-slate-500 text-white border-cyan-400">Please Click for Tab-Forms</button>
+        <button onClick={handleSearchBar} className="border-2 p-2 bg-slate-500 text-white border-cyan-400">Please Click for AutoComplete Search Bar</button>
+
 
       </main>
     </div>
